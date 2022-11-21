039df15deecfb5cd8c760c5a0ae6b41b65b04eab
AntoninoIrrera <irrera.antonio2003@gmail.com>
2022-11-21 16:54:25 +0100

    correzione nome variabile

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 const numChilo = prompt("inserisci quanti chilometri vuoi percorrere");
-const età = prompt("inserisci la tua età");
+const age = prompt("inserisci la tua età");
 
 let costoBiglietto = 0.21 * numChilo;
 
-if (età < 18){
+if (age < 18){
     let scontoBigliettoMinorenni = ((costoBiglietto * 20) / 100);
     let costoBigliettoMinorenni = costoBiglietto - scontoBigliettoMinorenni;
     document.getElementById("prezzoBiglietto").innerHTML = "Il costo del tuo biglietto sarà: " + costoBigliettoMinorenni.toFixed(2) + " euro";
 }
-else if (età >= 65){
+else if (age >= 65){
     let scontoBigliettoOver = ((costoBiglietto * 40) / 100);
     let costoBigliettoOver = costoBiglietto - scontoBigliettoOver;
     document.getElementById("prezzoBiglietto").innerHTML = "Il costo del tuo biglietto sarà: " + costoBigliettoOver.toFixed(2) + " euro";
